e2c873eb87a95445240b57f7036795db3d7c1f35
xue <753397215@qq.com>
2015-12-14 14:24:59 +0800

    xue1214

--- xuedongdong/my_blog/my_blog/urls.py
+++ xuedongdong/my_blog/my_blog/urls.py
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),  
     url(r'^$', 'article.views.home'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
     #url(r'^(\d+)/$', 'article.views.detail', name='detail'),
     #url(r'^test/$', 'article.views.test'),
 )
